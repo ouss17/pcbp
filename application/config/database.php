e9a34aa0ad6084e9ee4717dec339f19d02a4f2ed
ousmane diarra <diarraousmane37@gmail.com>
2020-01-12 10:43:13 +0100

    first commit

---
+++ application/config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Database configuration settings used by PDO.
+ */
+
+$config['dsn']      = 'mysql:host=localhost;dbname=coca4you';
+$config['password'] = '';
+$config['user']     = 'root';
+
+// $pdo = new PDO('mysql:host=localhost;dbname=coca4you','root','');
